25cf61a63880081b8b9d1844a3cfe92cb1d99374
kuy <kuy472013@gmail.com>
2020-09-26 10:18:05 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'; 
+
+function Dev(props) {
+  return <h1>Develop {props.nickname}</h1>
+}
+
+function Test({ name, description, rating }) {
+  return <div>
+    <h1>{name}</h1>
+    <span>{description}</span><br/>
+    <span>{rating}/5.0</span>
+  </div>
+}
+
+Test.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+}
+
+const testList = [
+  {
+    id:1,
+    name: "a",
+    description: "test_a",
+    rating:5,
+  },
+  {
+    id:2,
+    name: "b",
+    description: "test_b",
+    rating:4,
+  },
+  {
+    id:3,
+    name: "c",
+    description: "test_c",
+    rating:3,
+  },
+]
+
+function renderTest(test) {
+  return <Test key={test.id} name={test.name} description={test.description} rating={test.rating} />;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <Dev name="kuy" nickname="kikaku"></Dev>
+      <Dev name="kim" nickname="ww"></Dev>
+      <Test description="description"></Test>
+      {testList.map(renderTest)}
     </div>
   );
 }
